80af4ea0a90dca8ee4642cd6b9733b07e5b26c57
neosandeep24 <103883917+neosandeep24@users.noreply.github.com>
2025-05-16 15:43:57 +0530

    Add files via upload

---
+++ Arduino Basics/Episode02_BULB/Episode02_BULB.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+ pinMode(13,OUTPUT);
+  digitalWrite(13,LOW);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ digitalWrite(13,HIGH);
+ delay(1000);
+ digitalWrite(13,LOW);
+ delay(1000);
+}
