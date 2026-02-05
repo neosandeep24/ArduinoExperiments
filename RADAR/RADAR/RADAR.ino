119f52bdfb6bdf3944fa22a7d4b538017948e870
neosandeep24 <103883917+neosandeep24@users.noreply.github.com>
2026-02-05 19:18:58 +0530

    Add files via upload

---
+++ RADAR/RADAR/RADAR.ino
@@ -0,0 +1,61 @@
+// please subscribe to my channel https://youtube.com/@theneostudios?si=cTavnV0uBUAKYJ0j 
+
+#include<Servo.h>
+
+#define trigPin 3
+#define echoPin 2
+
+long duration;
+int distance ;
+
+Servo myservo;
+
+int calculateDistance()
+{
+  digitalWrite(trigPin,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin,LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration*0.034/2;
+  return distance;
+}
+
+void setup()
+{
+  pinMode(trigPin , OUTPUT);
+  pinMode(echoPin, INPUT);
+  myservo.attach(9);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+ int i ;
+ for (i=15; i<=165; i++)
+ {
+  myservo.write(i);
+  delay(15);
+  calculateDistance();
+  Serial.print(i);
+  Serial.print(",");
+  Serial.print(distance);
+  Serial.print(".");
+ }
+ for(i=165; i>=15; i--)
+ {
+  myservo.write(i);
+  delay(15);
+  calculateDistance();
+  Serial.print(i);
+  Serial.print(",");
+  Serial.print(distance);
+  Serial.print(".");
+  
+ }
+
+ 
+  
+
+}
